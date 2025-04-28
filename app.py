32b74bfce7bca8d14fc2a1a22b104927cc9a7732
mrske <wie.maximilian@gmail.com>
2025-04-28 21:54:13 +0200

    port change for flask

--- app.py
+++ app.py
@@ -28,4 +28,6 @@ def mensa_heute():
 # Hier noch deine Funktion `get_mensa_today_and_tomorrow_filtered()` reinkopieren
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
